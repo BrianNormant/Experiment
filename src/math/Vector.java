a54b616e3da9be470280e3b50b0ae7f1e9c03153
BrianNormant <75326066+BrianNormant@users.noreply.github.com>
2021-01-27 10:10:00 -0500

    Update Vector.java

--- src/math/Vector.java
+++ src/math/Vector.java
@@ -1,4 +1,4 @@
-package engine.math;/*
+package math;/*
  * Copyright (c) 2020. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
  * Morbi non lorem porttitor neque feugiat blandit. Ut vitae ipsum eget quam lacinia accumsan.
  * Etiam sed turpis ac ipsum condimentum fringilla. Maecenas magna.
