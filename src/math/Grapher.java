79963a0f7aefb84ca5f9b4f03e8da0c155d0db94
BrianNormant <brian.normant@gmail.com>
2021-02-26 00:04:07 -0500

    Corriger un bug dans le Grapher.java

--- src/math/Grapher.java
+++ src/math/Grapher.java
@@ -10,9 +10,10 @@ import java.util.Arrays;
  */
 public class Grapher extends JPanel {
     private final ArrayList<IFunction> functions = new ArrayList<>();
-    double x1, y1, x2, y2;
+    Double x1, y1, x2, y2;
 
-    public Grapher(int x1, int x2,int y1,int y2) {
+    public Grapher(Double x1, Double x2, Double y1, Double y2) {
+        setBorder(BorderFactory.createLineBorder(Color.black));
         this.x1 = x1;
         this.x2 = x2;
         this.y1 = y1;
@@ -20,53 +21,52 @@ public class Grapher extends JPanel {
     }
 
     public Dimension getPreferredSize() {
-        return new Dimension(250,200);
+        return new Dimension(200, 200);
     }
 
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
         g.drawLine(// X axis
-                normalizeX(x1),normalizeY((y2-y1)/2),
-                normalizeX(x2), normalizeY((y2-y1)/2)
+                normalizeX(x1), normalizeY((y2+y1)/2),
+                normalizeX(x2), normalizeY((y2+y1)/2)
         );
         g.drawLine(
-                normalizeX((x1-x2)/2),normalizeY(0),
-                normalizeX((x1-x2)/2),normalizeY(y1)
+                normalizeX((x1+x2)/2), normalizeY(y1),
+                normalizeX((x1+x2)/2), normalizeY(y2)
         );
-
+        g.setColor(Color.BLUE);
         for (IFunction f : functions) {
             for (int i = 0; i < this.getSize().width; i++) {
                 g.drawLine(
-                            i,normalizeY(f.evaluate(unNormalizeX(i))),
-                        i+1,normalizeY(f.evaluate(unNormalizeX(i+1)))
+                        i, normalizeY(-f.evaluate(unNormalizeX(i)+x1)),
+                        i + 1, normalizeY(-f.evaluate(unNormalizeX(i + 1)+x1))
                 );
             }
         }
 
     }
-    public void add(IFunction... f){
+
+    public void add(IFunction... f) {
         functions.addAll(Arrays.asList(f));
     }
+
     public void remove(IFunction f) {
         functions.remove(f);
     }
 
     public int normalizeX(double value) {
-        return (int)(value*this.getSize().width/x2-x1);
+        return (int) ((value+x2) * this.getSize().width / (x2 - x1));
     }
+
     public int normalizeY(double value) {
-        return -(int)(value*this.getSize().height/y2-y1);
+        return (int) ((value+y2) * this.getSize().height / (y2 - y1));
     }
+
     public double unNormalizeX(int value) {
-        return (value*(x2-x1)/(double)this.getSize().width)-x1;
+        return (value * (x2 - x1) / (double) this.getSize().width);
     }
+
     public double unNormalizeY(int value) {
-        return (-value*(y2-y1)/(double)this.getSize().height)-y1;
-    }
-    public int[] placeCoord(double x, double y) {
-        return new int[]{
-                normalizeX(x),//x
-                normalizeY(y)//y
-        };
+        return (value * (y2 - y1) / (double) this.getSize().height);
     }
 }
